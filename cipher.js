eea81279b663ae1e94cd839122a9612079e17bd2
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2023-12-23 04:35:04 -0500

    started conditionational logic for ascii char values.

--- cipher.js
+++ cipher.js
@@ -12,12 +12,31 @@ function cipherInput(input) {
 
     for (const char of inputStack) {
 
+        let characterAsciiCode = char.charCodeAt();
 
-        console.log(`char: ${char} \n ASCII value ${char.charCodeAt()}`);
+        console.log(`char: ${char} \n ASCII value ${char.charCodeAt()} \n`);
+
+        if (characterAsciiCode <= 90 && characterAsciiCode >=78) {
+
+            console.log("It's in the second half holmes! \n");
+
+            console.log(`the new char should be ${characterAsciiCode - 13}`)
+
+        } else if (characterAsciiCode <=77 && characterAsciiCode >=65) {
+
+            console.log("Yo it's in the first half holmes! \n")
+
+        } else {
+            console.log("Special Character dude! \n")
+        }
+
+
 
 
 
     }
+
+    console.log()
 }
 
 
